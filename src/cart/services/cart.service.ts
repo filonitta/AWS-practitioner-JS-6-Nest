717736fbc680b2604665af3c522fbe35cc98fa12
Nataliia Shyriaieva <Nataliia_Shyriaieva@epam.com>
2024-05-07 12:26:16 +0300

    add RDS and pg

--- src/cart/services/cart.service.ts
+++ src/cart/services/cart.service.ts
@@ -2,54 +2,62 @@ import { Injectable } from '@nestjs/common';
 
 import { v4 } from 'uuid';
 
+import { Client } from 'pg';
 import { Cart } from '../models';
 
 @Injectable()
 export class CartService {
-  private userCarts: Record<string, Cart> = {};
+  private client: Client;
 
-  findByUserId(userId: string): Cart {
-    return this.userCarts[ userId ];
+  constructor() {
+    this.client = new Client({
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    });
+
+    this.client.connect();
   }
 
-  createByUserId(userId: string) {
-    const id = v4(v4());
-    const userCart = {
-      id,
-      items: [],
-    };
-
-    this.userCarts[ userId ] = userCart;
-
-    return userCart;
+  async findByUserId(userId: string): Promise<Cart> {
+    const queryResult = await this.client.query(
+      'SELECT * FROM carts WHERE user_id = $1',
+      [userId],
+    );
+    return queryResult.rows[0]; // Assuming each user could only have one cart
   }
 
-  findOrCreateByUserId(userId: string): Cart {
-    const userCart = this.findByUserId(userId);
+  async createByUserId(userId: string): Promise<Cart> {
+    const id = v4();
+    await this.client.query(
+      `INSERT INTO carts (id, user_id, items) VALUES ($1, $2, ARRAY[]::uuid[]);`,
+      [id, userId],
+    );
+    return this.findByUserId(userId);
+  }
 
-    if (userCart) {
-      return userCart;
+  async findOrCreateByUserId(userId: string): Promise<Cart> {
+    const cart = await this.findByUserId(userId);
+
+    if (!cart) {
+      return await this.createByUserId(userId);
     }
 
-    return this.createByUserId(userId);
+    return cart;
   }
 
-  updateByUserId(userId: string, { items }: Cart): Cart {
-    const { id, ...rest } = this.findOrCreateByUserId(userId);
+  async updateByUserId(userId: string, { items }: Cart): Promise<Cart> {
+    await this.client.query('UPDATE carts SET items = $1 WHERE user_id = $2', [
+      items,
+      userId,
+    ]);
 
-    const updatedCart = {
-      id,
-      ...rest,
-      items: [ ...items ],
-    }
-
-    this.userCarts[ userId ] = { ...updatedCart };
-
-    return { ...updatedCart };
+    return this.findByUserId(userId);
   }
 
-  removeByUserId(userId): void {
-    this.userCarts[ userId ] = null;
+  async removeByUserId(userId: string): Promise<void> {
+    await this.client.query('DELETE FROM carts WHERE user_id = $1', [userId]);
   }
-
 }
--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 
-import * as helmet from 'helmet';
-
 import { AppModule } from './app.module';
 import serverlessExpress from '@codegenie/serverless-express';
 import { Callback, Context, Handler } from 'aws-lambda';
 
-const port = process.env.PORT || 4000;
 let server: Handler;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // app.enableCors({
-  //   origin: (req, callback) => callback(null, true),
-  // });
-  // app.use(helmet());
-
-  // await app.listen(port);
   await app.init();
 
   const expressApp = app.getHttpAdapter().getInstance();
   return serverlessExpress({ app: expressApp });
 }
-// bootstrap().then(() => {
-//   console.log('App is running on %s port', port);
-// });
 
 export const handler: Handler = async (
   event: any,
