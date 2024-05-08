8619d152831212c925f3ce10b2ae123c880ff498
Nataliia Shyriaieva <Nataliia_Shyriaieva@epam.com>
2024-05-08 15:16:29 +0300

    add database module

---
+++ src/database/database.providers.ts
@@ -0,0 +1,33 @@
+import { Provider } from '@nestjs/common';
+import { Client } from 'pg';
+
+export const DatabaseProviders: Provider[] = [
+  {
+    provide: 'DATABASE_CONNECTION',
+    useFactory: async (): Promise<any> => {
+      try {
+        const client = new Client({
+          host: process.env.DB_HOST,
+          port: Number(process.env.DB_PORT),
+          database: process.env.DB_NAME,
+          user: process.env.DB_USER,
+          password: process.env.DB_PASSWORD,
+          // ssl: {
+          //   rejectUnauthorized: false,
+          // },
+          // ssl: true,
+          // connectionTimeoutMillis: 5000,
+        });
+
+        await client.connect();
+        return client;
+      } catch (error) {
+        console.error(
+          'An error occurred in creating DATABASE_CONNECTION',
+          error,
+        );
+        throw error;
+      }
+    },
+  },
+];
